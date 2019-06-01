a19358de3b4f5b9ab76496df9d1d4b743a210572
Olaitanbosun <50694056+Olaitanbosun@users.noreply.github.com>
2019-06-01 10:17:36 +0100

    Add files via upload

--- js/department.js
+++ js/department.js
@@ -1,10 +1,9 @@
-var app = angular.module('dclm-app', []);
+var baseUrl = "http://10.10.11.185:8000/api/";
 
-var baseUrl = "http://localhost:8000/api/";
-
-app.controller('Department', function($scope, $http, $filter){
-    this.department = {dept_name:'', dept_code:''}
-    $scope.save = function(){
+app.controller('departmentController', function($scope, $http, $filter){
+    this.department = {department_name:'', department_code:''}
+    
+        $scope.save = function(){
         $http({
             url: baseUrl+'departments',
             method: "POST",
@@ -13,7 +12,70 @@ app.controller('Department', function($scope, $http, $filter){
         }) .then((response) =>{
             $scope.info = response.data.message;
         }, function(error){
-            $scope.error = error.data.message;
-        })
+            $scope.error = error.data.message
+             });
+         }
+    
+    $scope.deletedepartment = function(id){
+    $http({
+    url: baseUrl + "departments/"+id,
+    method: "DELETE",
+    contentType: 'application/json'
+    }).then((data) => {
+    $scope.info = data.data.message;
+    setTimeout(window.location.reload(), 1000);
+    }, (error) => {
+    $scope.uerror = error
+})
     }
 })
+
+app.controller('departmentlist', function ($scope, $http) {
+$http.get(baseUrl + 'departments')
+.then((data) => {
+$scope.departments = data.data;
+}, (err) => {
+$scope.error = err;
+});
+})
+
+app.controller('viewDepartmentController', function($scope, $http, $routeParams){
+var id = $routeParams.department;
+$http.get(baseUrl + "departments/" + id)
+.then ((data) => {
+console.log('data:', data.data)
+$scope.dept = data.data;
+}, (err) => {
+    $scope.verror = err;
+    console.log(err)
+})
+
+})
+app.controller('editDepartmentController', function($scope, $http, $routeParams){
+    this.department = {department_name:'', department_code:''}
+    var id = $routeParams.department;
+    $http.get(baseUrl + "departments/" + id)
+    .then ((data) => {
+    $scope.department = data.data;
+    $scope.edit = 1;
+    }, (err) => {
+        $scope.ederror = err;
+    })
+    
+    $scope.update = function() {
+        
+        $http({
+        url: baseUrl+"departments/"+id,
+        method: "PUT",
+        data: this.department,
+        contentType: 'application/json'
+        }).then ((data) => {
+        $scope.info = data.data.message
+        console.log(data.data.messsage)
+        }, (error) => {
+        $scope.ederror = error
+        console.log(error)
+    })
+    }
+    })
+
